6c3b4d5f8d0cbd262a7b4284d6c56ac8b04ec220
Martino Facchin <m.facchin@arduino.cc>
2017-03-22 11:46:41 +0100

    Avoid board change during compilation/upload
    By threading the boardChange callback we can busy wait until
    the compilation/upload phase has ended and change the board when done.
    
    Fixes #6035

--- app/src/processing/app/Editor.java
+++ app/src/processing/app/Editor.java
@@ -2146,6 +2146,10 @@ public class Editor extends JFrame implements RunnerListener {
     }
   }
 
+  public boolean isCompiling() {
+    return uploading;
+  }
+
   private void resumeOrCloseSerialMonitor() {
     // Return the serial monitor window to its initial state
     if (serialMonitor != null) {
